ae22a40634d04466e0971733a7292a901fbe89c3
Fábio Ribeiro <68775701+fabiorisantos@users.noreply.github.com>
2022-12-11 00:48:37 +0000

    Equality

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -9,24 +9,24 @@ suite('Unit Tests', function () {
       assert.isNotNull(1, '1 is not null');
     });
     // #2
-//    test('#isDefined, #isUndefined', function () {
-//      assert.fail(null, 'null is not undefined');
-//      assert.fail(undefined, 'undefined IS undefined');
-//      assert.fail('hello', 'A string is not undefined');
-//    });
+    test('#isDefined, #isUndefined', function () {
+      assert.isDefined(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'A string is not undefined');
+    });
     // #3
-//    test('#isOk, #isNotOk', function () {
-//      assert.fail(null, 'null is falsey');
-//      assert.fail("I'm truthy", 'A string is truthy');
-//      assert.fail(true, 'true is truthy');
-//    });
+    test('#isOk, #isNotOk', function () {
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isOk(true, 'true is truthy');
+    });
     // #4
-//    test('#isTrue, #isNotTrue', function () {
-//      assert.fail(true, 'true is true');
-//      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-//      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
-//    });
-//  });
+    test('#isTrue, #isNotTrue', function () {
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+    });
+  });
 
   // -----------------------------------------------------------------------------
 
@@ -165,4 +165,4 @@ suite('Unit Tests', function () {
 //  });
 
   // -----------------------------------------------------------------------------
-});
+//});
